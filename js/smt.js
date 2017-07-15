210e376d5f8363f2be88ac8cea7121f4176cf234
santy_naren <santhoshnarendra@gmail.com>
2017-07-15 21:01:01 +0530

    update

--- js/smt.js
+++ js/smt.js
@@ -6,15 +6,16 @@ $(document).ready(function(){
      $('.counter').html(scrollTop);
     
     if (scrollTop >= 100) {
+       
       $('.navbar-brand').addClass('scrolled-nav');
       $('.navbar-collapse').addClass('scrolled-nav-collapse');
-      $('#font_logo').show();  
-      $('#img_logo').hide();  
+      $('#font_logo').show(1000);  
+      $('#img_logo').hide(1000);  
     } else if (scrollTop < 100) {
       $('.navbar-brand').removeClass('scrolled-nav');
       $('.navbar-collapse').removeClass('scrolled-nav-collapse');
-      $('#font_logo').hide();  
-      $('#img_logo').show(); 
+      $('#font_logo').hide(1000);  
+      $('#img_logo').show(1000); 
     } 
     
   }); 
